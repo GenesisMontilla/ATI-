86507ab381391ef786ef548a64eeed42a3e664ed
GenesisMontilla <genesisvaleriamontilla@gmail.com>
2026-05-19 19:20:19 -0400

    listado de perfiles dinámico usando js - quité lo que hacia falta de profile e index

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log(" La configuracionn funcionaaa", config);
+
+window.addEventListener('DOMContentLoaded', () => {
+    const contenedor = document.querySelector('.student-grid'); 
+    contenedor.innerHTML = ''; 
+
+    profiles.forEach(estudiante => {
+        const tarjeta = document.createElement('div');
+        tarjeta.className = 'student-card';
+        tarjeta.style.cursor = 'pointer'; 
+        
+        
+        tarjeta.innerHTML = `
+            <img src="${estudiante.ci}/${estudiante.ci}Big${estudiante.image_ext}" alt="${estudiante.name}" class="imagen-perfil">
+            <div class="info">
+                <p>${estudiante.name}</p>
+                <div class="blue-bar"></div>
+            </div>
+        `;
+        
+        tarjeta.addEventListener('click', () => {
+            window.location.href = `profile.html?ci=${estudiante.ci}`;
+        });
+        
+        contenedor.appendChild(tarjeta);
+    });
 });
