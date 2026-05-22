b4cd4b933d2af97a06b1fe4bd79d1815e83abe1d
GenesisMontilla <genesisvaleriamontilla@gmail.com>
2026-05-21 20:01:04 -0400

    cambio de lenguaje según atributo

--- js/index.js
+++ js/index.js
@@ -1,26 +1,70 @@
+const urlParams = new URLSearchParams(location.search);
+const langParam = urlParams.get("lang");
 
-window.addEventListener('DOMContentLoaded', () => {
-    const contenedor = document.querySelector('.student-grid'); 
-    contenedor.innerHTML = ''; 
+if (!langParam) {
+    urlParams.set("lang", "ES"); 
+    window.history.replaceState({}, '', `${window.location.pathname}?${urlParams.toString()}`);
+}
+
+const lang = new URLSearchParams(location.search).get("lang") || "ES";
+
+const c = document.createElement("script");
+c.src = `conf/config${lang.toUpperCase()}.json`;
+
+c.onload = () => {
+
+    
+    document.getElementById("search").placeholder = config.search;
+
+    const contenedor = document.querySelector('.student-grid');
+    contenedor.innerHTML='';
+
+    const $=(id,v)=>document.getElementById(id).innerHTML=v;
+    const setPlaceholder = (id, v) => document.getElementById(id).placeholder = v;
+
+    function renderInterfaz(d) {
+            setPlaceholder("search", config.search);
+            $("semestre", d.semester)
+            $("search-button", d.search);
+            $("footer", d.copyRight);
+
+    }
+
+        if(typeof config != "undefined"){
+            renderInterfaz(config);
+        }
+
+       
 
     profiles.forEach(estudiante => {
-        const tarjeta = document.createElement('div');
-        tarjeta.className = 'student-card';
-        tarjeta.style.cursor = 'pointer'; 
-        
-        
-        tarjeta.innerHTML = `
-            <img src="${estudiante.ci}/${estudiante.ci}Big${estudiante.image_ext}" alt="${estudiante.name}" class="imagen-perfil">
-            <div class="info">
-                <p>${estudiante.name}</p>
-                <div class="blue-bar"></div>
-            </div>
+
+        const tarjeta=document.createElement('div');
+        tarjeta.className='student-card';
+        tarjeta.style.cursor='pointer';
+
+        tarjeta.innerHTML=`
+        <img src="${estudiante.ci}/${estudiante.ci}Big${estudiante.image_ext}" 
+        alt="${estudiante.name}"
+        class="imagen-perfil">
+
+        <div class="info">
+            <p>${estudiante.name}</p>
+            <div class="blue-bar"></div>
+        </div>
         `;
-        
-        tarjeta.addEventListener('click', () => {
-            window.location.href = `profile.html?ci=${estudiante.ci}`;
+
+        tarjeta.addEventListener("click",()=>{
+
+            window.location.href=
+            `profile.html?ci=${estudiante.ci}&lang=${lang}`;
+
         });
-        
+
         contenedor.appendChild(tarjeta);
+
+
     });
-});
+
+};
+
+document.head.appendChild(c);
--- js/profile.js
+++ js/profile.js
@@ -1,36 +1,54 @@
+const lang = new URLSearchParams(location.search).get("lang") || "ES";
 
+const ci= new URLSearchParams(location.search).get("ci");
 
- const ci = new URLSearchParams(location.search).get("ci");
+const c=document.createElement("script");
 
-const s = document.createElement("script");
-s.src = `${ci}/profile.json`;
+c.src=`conf/config${lang.toUpperCase()}.json`;
 
-s.onload = () => {
-    if (typeof profile != "undefined")
-        render(profile);
+c.onload=()=>{
+
+    const s=document.createElement("script");
+
+    s.src=`${ci}/profile.json`;
+
+    s.onload=()=>{
+
+        if(typeof profile!="undefined"){
+            render(profile);
+        }
+
+    };
+
+    document.head.appendChild(s);
+
+    
+
 };
 
-document.head.appendChild(s);
+document.head.appendChild(c);
 
 
 function render(d){
 
-    const $=(id,v)=>document.getElementById(id).innerHTML=v;
-
+    const $=(id,v)=> document.getElementById(id).innerHTML=v;
+    
+    const setPlaceholder = (id, v) => document.getElementById(id).placeholder = v;
+        
+    
     document.getElementById("profile-picture").src=
     `${d.ci}/${d.ci}Big.jpg`;
 
     $("nombre",d.name);
+
     $("descripcion",d.description);
 
     $("email",
-      config.email.replace("[email]",d.email));
+    config.email.replace("[email]",d.email));
 
-    $("ci","Cédula: "+d.ci);
-    $("genero","Género: "+d.gender);
-    $("cumpleaños","Fecha de nacimiento: "+d.birth_date);
 
-    $("color",config.color+": "+d.color);
+    $("color",
+    config.color+": "+d.color);
 
 
     function texto(lista,tipo){
@@ -38,16 +56,24 @@ function render(d){
         return lista.length>1
         ? config[tipo][1]+": "+lista.join(", ")
         : config[tipo][0]+": "+lista[0];
-    }
 
+    }
 
     $("libro",texto(d.book,"book"));
 
-    $("musica",texto(d.music,"music"));
+    $("musica",
+    texto(d.music,"music"));
 
-    $("videojuego",texto(d.video_game,"video_game"));
+    $("videojuego",
+    texto(d.video_game,"video_game"));
 
     $("lenguajes",
-        config.language+": "+
-        d.language.join(", "));
+    config.language+": "+
+    d.language.join(", "));
+
+    $("footer",config.copyRight);
+    setPlaceholder("busqueda",config.search);
+    $("boton-busqueda",config.search);
+
+
 }
